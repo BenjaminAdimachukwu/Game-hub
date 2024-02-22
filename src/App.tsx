62a4f0cbc34fa0bb65fec884e415d8c95d890cf4
Adimachukwu Benjamin <thegreatben27@gmail.com>
2024-02-22 23:41:25 +0100

    build SortSelector

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,67 @@
-import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import GamGrid from "./components/GamGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import PlatFormSelector from "./components/PlatFormSelector";
+import SortSelector from "./components/SortSelector";
 interface Genre {
   id: number;
   name: string;
-  image_background:string
-  }
-  interface Platform {
-    id: number;
-    name: string;
-    slug: string
+  image_background: string;
+}
+interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 
 interface GameQuery {
   genre: Genre | null;
-  platform: Platform | null
+  platform: Platform | null;
 }
 function App() {
   // const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
   // const [selectedPlatform, setselectedPlatform]= useState<Platform | null>(null)
   //the above was commented due to code refactoring
 
-  const [GameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery)
+  const [GameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
   return (
-    <Grid templateAreas={{
-      base: `"nav" "main"`,
-      lg: `"nav nav" "aside main"`
-    }}
-    
-    templateColumns={{
-base: '1fr',
-lg: '200px 1fr'
-    }}
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
-      
       <GridItem area="nav">
-        <Navbar/>
+        <Navbar />
       </GridItem>
       <Show above="lg">
-      <GridItem area="aside" paddingX={5}>
-        {/* <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre)=> setSelectedGenre(genre)}/> */}
-        <GenreList selectedGenre={GameQuery.genre} onSelectGenre={(genre)=> setGameQuery({...GameQuery, genre})}/>
-      </GridItem>
+        <GridItem area="aside" paddingX={5}>
+          {/* <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre)=> setSelectedGenre(genre)}/> */}
+          <GenreList
+            selectedGenre={GameQuery.genre}
+            onSelectGenre={(genre) => setGameQuery({ ...GameQuery, genre })}
+          />
+        </GridItem>
       </Show>
       <GridItem area="main">
         {/* <PlatFormSelector selectedPlatform={selectedPlatform} onSelectPlatForm={(platform)=> setselectedPlatform(platform)}/> */}
-        <PlatFormSelector selectedPlatform={GameQuery.platform} onSelectPlatForm={(platform)=> setGameQuery({...GameQuery, platform})}/>
+        <HStack spacing={5} paddingLeft={5} marginBottom={5}> 
+        <PlatFormSelector
+          selectedPlatform={GameQuery.platform}
+          onSelectPlatForm={(platform) =>
+            setGameQuery({ ...GameQuery, platform })
+          }
+          />
+        <SortSelector/>
+          </HStack>
         {/* <GamGrid selectedGenre={selectedGenre} selectedPlatform={selectedPlatform} /> */}
-        <GamGrid gameQuery={GameQuery}/>
+        <GamGrid gameQuery={GameQuery} />
       </GridItem>
     </Grid>
   );
--- src/components/PlatFormSelector.tsx
+++ src/components/PlatFormSelector.tsx
@@ -1,28 +1,36 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
-import React from 'react'
-import { BsChevronDown } from 'react-icons/bs'
-import usePlatForms from '../hooks/usePlatforms'
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import React from "react";
+import { BsChevronDown } from "react-icons/bs";
+import usePlatForms from "../hooks/usePlatforms";
 interface Platform {
-    id: number;
-    name: string;
-    slug: string
+  id: number;
+  name: string;
+  slug: string;
 }
-interface Props{
-    onSelectPlatForm: (platform: Platform)=> void
-    selectedPlatform : Platform | null
+interface Props {
+  onSelectPlatForm: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
 }
-const PlatFormSelector = ({onSelectPlatForm, selectedPlatform}: Props) => {
-    const { data, error} = usePlatForms()
-    if(error) return null
+const PlatFormSelector = ({ onSelectPlatForm, selectedPlatform }: Props) => {
+  const { data, error } = usePlatForms();
+  if (error) return null;
   return (
     <Menu>
-        <MenuButton as={Button} rightIcon={<BsChevronDown/>}>{selectedPlatform?.name || 'platform'}</MenuButton>
-        <MenuList>
-            {data.map(platform => <MenuItem key={platform.id} onClick={()=> (onSelectPlatForm(platform))}>{platform.name}</MenuItem>)}
-            
-        </MenuList>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        {selectedPlatform?.name || "platform"}
+      </MenuButton>
+      <MenuList>
+        {data.map((platform) => (
+          <MenuItem
+            key={platform.id}
+            onClick={() => onSelectPlatForm(platform)}
+          >
+            {platform.name}
+          </MenuItem>
+        ))}
+      </MenuList>
     </Menu>
-  )
-}
+  );
+};
 
-export default PlatFormSelector
+export default PlatFormSelector;
